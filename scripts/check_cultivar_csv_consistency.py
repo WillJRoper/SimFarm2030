aafba7b42f71ea12cec100dc4750f213a857955b
Grahame Gardiner <grahamegee@gmail.com>
2021-02-03 15:00:19 +0000

    Generate cleaned ripe time and yield datasets
    The first stage of cleaning is to get them sorted by region

---
+++ scripts/check_cultivar_csv_consistency.py
@@ -0,0 +1,17 @@
+from os.path import abspath, dirname, join
+import pandas as pd
+
+PARENT_DIR = dirname(dirname(abspath(__file__)))
+YIELD_PATH = join(PARENT_DIR, "All_Cultivars_Spreadsheets", "yield_cleaned.csv")
+RIPETIME_PATH = join(PARENT_DIR, "All_Cultivars_Spreadsheets", "ripe_time_cleaned.csv")
+
+
+with open(RIPETIME_PATH) as rf, open(YIELD_PATH) as yf:
+    rt_df = pd.read_csv(rf)
+    y_df = pd.read_csv(yf)
+
+    rt_comp_df = rt_df[["Region", "Lat", "Long", "Year"]].reset_index(drop=True)
+    y_comp_df = y_df[["Region", "Lat", "Long", "Year"]].reset_index(drop=True)
+
+    eq = rt_comp_df == y_comp_df
+    print("hi")
